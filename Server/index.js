6308042c9cca4161c11d2b01118ccc4f1eecd16a
Rober P <robertopaletini@gmail.com>
2021-12-14 19:33:40 -0300

    hola

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require("./config");
 const likeRoute = require("./Routes/likeMethods");
 const rankRoute = require("./Routes/rankMethods");
-const uri = config.getSecretToken();
 
 const app = express();
-const Port = process.env.PORT || 3600;
+const Port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,15 +17,5 @@ app.get("/", (req, res, next) => {
 
 app.use("/likes", likeRoute);
 app.use("/ranks", rankRoute);
-app.listen(Port, () => {
-  console.log(`Server Running ${Port}`);
-});
+app.listen(process.env.PORT || 3000);
 
-mongoose
-  .connect(uri)
-  .then(function () {
-    console.log("Moongose Connection Successful");
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
